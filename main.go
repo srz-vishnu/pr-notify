e0e1602390a9ba325a050c53c70f537286ef9605
srz-vishnu <vishnuk@smartrabbitz.com>
2025-04-25 16:09:50 +0530

    adding yml file to do code check and take build, plus an yml file which does this github action

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println("to check how yml file gonna run it on github action")
+	fmt.Printlnnnnnn("lets create a syntax error", mistake)
+}
